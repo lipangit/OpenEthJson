483af6027396adb6e998c05b4356d2afd496edd2
lipangit <1066666651@qq.com>
2022-10-14 20:01:19 +0800

    can open ethjson with keywords

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 const web3 = require('web3');
 const fs = require('fs');
+const { exit } = require('process');
 
 var KeyPath = '';
 var KeyData = '';
@@ -12,30 +13,34 @@ KeyPath = process.argv[2];
 times = process.argv[3];
 console.log('KeyPath', KeyPath);
 console.log('times', times);
+KeyWords.push('');
 process.argv.forEach((val, index) => {
-    if (index > 2) {
+    if (index > 3) {
         KeyWords.push(val);
     }
 });
 console.log('KeyWords', KeyWords);
 
 fs.readFile(KeyPath, 'utf-8', ((err, data) => {
-    // console.log('KeyData', data);
     KeyData = data;
-    var web33 = new web3(web3.givenProvider || "ws://localhost:8555");//密码正确和这个没有关系。
+    console.log('KeyData', KeyData);
+    var w3 = new web3(web3.givenProvider || "ws://localhost:8555");//密码正确和这个没有关系。
 
-
-
-    try {
-        var ret = web33.eth.accounts.decrypt(KeyData, password);
-        console.log("succ");
-    }
-    catch (e) {
-        console.log("failed");
-    }
-
-    console.log("final");
+    KeyWords.forEach((key, index) => {
+        KeyWords.forEach((keyword, index) => {
+            for (var i = 0; i < times; i++) {
+                password = password + key + keyword;
+            }
+            console.log(password);
+            try {
+                var ret = w3.eth.accounts.decrypt(KeyData, password);
+                console.log("succ");
+                exit(0);
+            }
+            catch (e) {
+                console.log("failed");
+            }
+            password = '';
+        })
+    })
 }));
-
-
-
