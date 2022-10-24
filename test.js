c506c188317f877dab9a77331cb6afff7df903dc
lipangit <1066666651@qq.com>
2022-10-24 22:22:55 +0800

    not good

--- test.js
+++ test.js
@@ -31,58 +31,57 @@ function backtrack(result, board, nums) {
         board.pop()
     });
 }
-backtrack(resultOut, boardOut, KeyWords)
-console.log(resultOut)
-
-
-
-
-
+// backtrack(resultOut, boardOut, KeyWords)
+// console.log(resultOut)
 
 //分成两步骤，
-// function firestStep(originArray) {
-//     var result = []
-//     var len = originArray.length
-//     var retSize = (1 << len) - 1
-//     for (i = 0; i < retSize; i++) {
-//         var singleArray = []
-//         var curPosition = len - 1
-//         var number = i
-//         while (number != 0) {
-//             if ((number & 1) == 1) {
-//                 singleArray[curPosition] = originArray[curPosition];
-//             }
-//             number = number >>> 1;  //无符号右移去除低位
-//             curPosition--;  //记录下一位
-//         }
-//         result.push(singleArray)
-//     }
-//     return result
-// }
+function firestStep(originArray) {
+    var result = []
+    var len = originArray.length
+    var retSize = (1 << len) - 1
+    for (i = 0; i < retSize; i++) {
+        var singleArray = []
+        var curPosition = len - 1
+        var number = i
+        while (number != 0) {
+            if ((number & 1) == 1) {
+                singleArray[curPosition] = originArray[curPosition];
+            }
+            number = number >>> 1;  //无符号右移去除低位
+            curPosition--;  //记录下一位
+        }
+        result.push(singleArray)
+    }
+    return result
+}
 
-// function removeEmptyItems(originArray) {
-//     var result = []
-//     for (i = 0; i < originArray.length; i++) {
-//         if (originArray[i] != null) {
-//             result.push(originArray[i])
-//         }
-//     }
-//     return result
-// }
+function removeEmptyItems(originArray) {
+    var result = []
+    for (i = 0; i < originArray.length; i++) {
+        if (originArray[i] != null) {
+            result.push(originArray[i])
+        }
+    }
+    return result
+}
 
-// function backTrack() {
-
+var result1 = firestStep(KeyWords)
+var result2 = []
+for (j = 0; j < result1.length; j++) {
+    if (result1[j] != "") {
+        var ret = removeEmptyItems(result1[j])
+        result2.push(ret)
+        // console.log(ret + " j " + j + " length:" + ret.length)
+    } else {
+        // console.log("kong:" + j)
+    }
+}
+console.log(result2)
+// var finalResult = []
+// for (i = 0; i < result2.length; i++) {
+//     backtrack(resultOut, boardOut, result2[i])
+//     var temp = resultOut.slice(0)
+//     boardOut = []
+//     resultOut = []
+//     console.log("temp: " + temp)
 // }
-
-// var result = firestStep(KeyWords)
-// var secResult = []
-// for (j = 0; j < result.length; j++) {
-//     if (result[j] != "") {
-//         var ret = removeEmptyItems(result[j])
-//         secResult.push(ret)
-//         // console.log(ret + " j " + j + " length:" + ret.length)
-//     } else {
-//         // console.log("kong:" + j)
-//     }
-// }
-// console.log(secResult)
